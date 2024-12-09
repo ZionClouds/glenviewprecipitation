006952bc47934615dc16feb05404b656cdab77c7
Soaham Joshi <soaham@zionclouds.com>
2024-12-09 14:03:58 -0600

    Add files via upload

---
+++ getData.php
@@ -0,0 +1,42 @@
+<?php
+// $hostserver = "localhost";
+// $username = "root";
+// $password = "";
+// $dbName = "glenview_weather";
+
+// $conn = new mysqli($hostserver, $username, $password, $dbName);
+// if($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sql = "SELECT * from preciptation";
+// $result = $conn->query($sql);
+
+// if($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         print_r($row);
+//     }
+// } else {
+//     echo "No Data";
+// }
+
+// $conn->close();
+
+$json = file_get_contents('data.json');
+if($json === false) {
+    die('Error reading the JSON file');
+}
+
+$json_data = json_decode($json, true);
+if($json_data === null) {
+    die("Error decoding the JSON file");
+}
+
+// echo "<pre>";
+// print_r($json_data);
+// echo "</pre>";
+
+header('Content-Type: application/json');
+echo json_encode($json_data);
+
+?>
